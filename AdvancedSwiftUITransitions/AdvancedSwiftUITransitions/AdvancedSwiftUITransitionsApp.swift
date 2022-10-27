f960a448bea8d54d41401b874b5b3338cbf2b3b0
mkotyak <kotyak.maria@gmail.com>
2022-10-27 11:20:31 +0200

    Added my project

---
+++ AdvancedSwiftUITransitions/AdvancedSwiftUITransitions/AdvancedSwiftUITransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUITransitionsApp.swift
+//  AdvancedSwiftUITransitions
+//
+//  Created by Maria Kotyak on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUITransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
